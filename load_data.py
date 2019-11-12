8217e9a070a14bcdc714cc2799d0216ad01e77d7
anspire <iamanspire@gmail.com>
2019-11-12 09:12:53 +0530

    updated lpnet

--- load_data.py
+++ load_data.py
@@ -2,135 +2,75 @@ from torch.utils.data import *
 from imutils import paths
 import cv2
 import numpy as np
+from os.path import isfile, join
+import os
+from PIL import Image
 
 
-class labelFpsDataLoader(Dataset):
-    def __init__(self, img_dir, imgSize, is_transform=None):
-        self.img_dir = img_dir
-        self.img_paths = []
-        for i in range(len(img_dir)):
-            self.img_paths += [el for el in paths.list_images(img_dir[i])]
-        # self.img_paths = os.listdir(img_dir)
-        # print self.img_paths
-        self.img_size = imgSize
-        self.is_transform = is_transform
+def load_path(img_dir):
+  files = os.listdir(img_dir)
+  files = list(filter(lambda a: a[-3:] == 'txt', files))
+  img_paths = []
+  boxes = []
+  lp_nums = []
+  for f in files:
+    fp= open(join(img_dir, f), "r")
+    text = fp.read()
+    content = text.strip().split('\t')
+    img_paths.append(content[0])
+    boxes.append(content[1:5])
+    lp_nums.append(content[-1])
+  return img_paths[:20], boxes[:20], lp_nums[:20]
 
-    def __len__(self):
-        return len(self.img_paths)
+class lpDataLoader(Dataset):
+  def __init__(self, img_dir, input_shape, ads, model_name = model_name, is_transform=None):
+    self.img_dir = img_dir
+    self.ads = ads
+    self.img_paths, self.boxes, self.lp_nums = load_path(self.img_dir)
+    self.input_shape = input_shape
+    self.model_name = model_name
+    self.is_transform = is_transform
 
-    def __getitem__(self, index):
-        img_name = self.img_paths[index]
-        img = cv2.imread(img_name)
-        # img = img.astype('float32')
-        resizedImage = cv2.resize(img, self.img_size)
-        resizedImage = np.transpose(resizedImage, (2,0,1))
-        resizedImage = resizedImage.astype('float32')
-        resizedImage /= 255.0
-        lbl = img_name.split('/')[-1].rsplit('.', 1)[0].split('-')[-3]
+  def __len__(self):
+    return len(self.img_paths)
 
-        iname = img_name.rsplit('/', 1)[-1].rsplit('.', 1)[0].split('-')
-        # fps = [[int(eel) for eel in el.split('&')] for el in iname[3].split('_')]
-        # leftUp, rightDown = [min([fps[el][0] for el in range(4)]), min([fps[el][1] for el in range(4)])], [
-        #     max([fps[el][0] for el in range(4)]), max([fps[el][1] for el in range(4)])]
-        [leftUp, rightDown] = [[int(eel) for eel in el.split('&')] for el in iname[2].split('_')]
-        ori_w, ori_h = [float(int(el)) for el in [img.shape[1], img.shape[0]]]
-        new_labels = [(leftUp[0] + rightDown[0]) / (2 * ori_w), (leftUp[1] + rightDown[1]) / (2 * ori_h),
-                      (rightDown[0] - leftUp[0]) / ori_w, (rightDown[1] - leftUp[1]) / ori_h]
+  def __getitem__(self, index):
+    img_name = self.img_paths[index]
+    image = Image.open(join(self.img_dir, img_name))
+    iw, ih = image.size
+    box =  np.array([int(i) for i in self.boxes[index]])
+    lp_num = self.lp_nums[index]
+    lp_num = [int(self.ads[lp_num[i]]) for i in range(len(lp_num))]
+    for i in range(10 - len(lp_num)):
+      lp_num.append(0)
+    h, w = input_shape
 
-        return resizedImage, new_labels, lbl, img_name
+    box[0] = box[0] - (box[2] / 2)
+    box[1] = box[1] - (box[3] / 2)
+    box[2] = box[0] + box[2]
+    box[3] = box[1] + box[3]
 
+    # resize image
+    scale = min(w/iw, h/ih)
+    nw = int(iw*scale)
+    nh = int(ih*scale)
+    dx = (w-nw)//2
+    dy = (h-nh)//2
 
-class labelTestDataLoader(Dataset):
-    def __init__(self, img_dir, imgSize, is_transform=None):
-        self.img_dir = img_dir
-        self.img_paths = []
-        for i in range(len(img_dir)):
-            self.img_paths += [el for el in paths.list_images(img_dir[i])]
-        # self.img_paths = os.listdir(img_dir)
-        # print self.img_paths
-        self.img_size = imgSize
-        self.is_transform = is_transform
+    image = image.resize((nw,nh), Image.BICUBIC)
+    new_image = Image.new('RGB', (w,h), (128,128,128))
+    new_image.paste(image, (dx, dy))
+    image_data = np.array(new_image)/255.
 
-    def __len__(self):
-        return len(self.img_paths)
+    box[[0,2]] = box[[0,2]] * scale + dx
+    box[[1,3]] = box[[1,3]] * scale + dy
+    bw = box[2] - box[0]
+    bh = box[3] - box[1]
+    cx = box[0] + (bw / 2)
+    cy = box[1] + (bh / 2)
 
-    def __getitem__(self, index):
-        img_name = self.img_paths[index]
-        img = cv2.imread(img_name)
-        # img = img.astype('float32')
-        resizedImage = cv2.resize(img, self.img_size)
-        resizedImage = np.transpose(resizedImage, (2,0,1))
-        resizedImage = resizedImage.astype('float32')
-        resizedImage /= 255.0
-        lbl = img_name.split('/')[-1].split('.')[0].split('-')[-3]
-        return resizedImage, lbl, img_name
-
-
-
-class ChaLocDataLoader(Dataset):
-    def __init__(self, img_dir,imgSize, is_transform=None):
-        self.img_dir = img_dir
-        self.img_paths = []
-        for i in range(len(img_dir)):
-            self.img_paths += [el for el in paths.list_images(img_dir[i])]
-        # self.img_paths = os.listdir(img_dir)
-        # print self.img_paths
-        self.img_size = imgSize
-        self.is_transform = is_transform
-
-    def __len__(self):
-        return len(self.img_paths)
-
-    def __getitem__(self, index):
-        img_name = self.img_paths[index]
-        img = cv2.imread(img_name)
-        resizedImage = cv2.resize(img, self.img_size)
-        resizedImage = np.reshape(resizedImage, (resizedImage.shape[2], resizedImage.shape[0], resizedImage.shape[1]))
-
-        iname = img_name.rsplit('/', 1)[-1].rsplit('.', 1)[0].split('-')
-        [leftUp, rightDown] = [[int(eel) for eel in el.split('&')] for el in iname[2].split('_')]
-
-        # tps = [[int(eel) for eel in el.split('&')] for el in iname[2].split('_')]
-        # for dot in tps:
-        #     cv2.circle(img, (int(dot[0]), int(dot[1])), 2, (0, 0, 255), 2)
-        # cv2.imwrite("/home/xubb/1_new.jpg", img)
-
-        ori_w, ori_h = float(img.shape[1]), float(img.shape[0])
-        assert img.shape[0] == 1160
-        new_labels = [(leftUp[0] + rightDown[0])/(2*ori_w), (leftUp[1] + rightDown[1])/(2*ori_h), (rightDown[0]-leftUp[0])/ori_w, (rightDown[1]-leftUp[1])/ori_h]
-
-        resizedImage = resizedImage.astype('float32')
-        # Y = Y.astype('int8')
-        resizedImage /= 255.0
-        # lbl = img_name.split('.')[0].rsplit('-',1)[-1].split('_')[:-1]
-        # lbl = img_name.split('/')[-1].split('.')[0].rsplit('-',1)[-1]
-        # lbl = map(int, lbl)
-        # lbl2 = [[el] for el in lbl]
-
-        # resizedImage = torch.from_numpy(resizedImage).float()
-        return resizedImage, new_labels
-
-
-class demoTestDataLoader(Dataset):
-    def __init__(self, img_dir, imgSize, is_transform=None):
-        self.img_dir = img_dir
-        self.img_paths = []
-        for i in range(len(img_dir)):
-            self.img_paths += [el for el in paths.list_images(img_dir[i])]
-        # self.img_paths = os.listdir(img_dir)
-        # print self.img_paths
-        self.img_size = imgSize
-        self.is_transform = is_transform
-
-    def __len__(self):
-        return len(self.img_paths)
-
-    def __getitem__(self, index):
-        img_name = self.img_paths[index]
-        img = cv2.imread(img_name)
-        # img = img.astype('float32')
-        resizedImage = cv2.resize(img, self.img_size)
-        resizedImage = np.transpose(resizedImage, (2,0,1))
-        resizedImage = resizedImage.astype('float32')
-        resizedImage /= 255.0
-        return resizedImage, img_name
+    new_label = [cx / w, cy / h, bw / w, bh / h]
+    if self.model_name == 'lpnet':
+      return image_data, new_label, lp_num, img_name
+    else:
+      return image_data, new_label
--- train.py
+++ train.py
@@ -2,63 +2,28 @@ from lpnet import lpnet
 from load_data import *
 
 from torch.utils.data import *
-from imutils import paths
-from os.path import isfile, join
-import cv2
-import os
-import numpy as np
 
 numPoints = 4
-numClasses = 35
-batchSize = 16
-imgSize = (480, 480)
+numClasses = 36
+batchSize = 32
+input_shape = (480, 480)
+model_name = 'base'
+img_dir = 'img'
+epochs = 25
+num_workers = 4
 
-def load_path(img_dir):
-    files = os.listdir(img_dir)
-    files = list(filter(lambda a: a[-3:] == 'txt', files))
-    img_paths = []
-    labels = []
-    for f in files:
-        fp= open(join(img_dir, f), "r")
-        text = fp.read()
-        content = text.strip().split('\t')
-        img_paths.append(content[0])
-        labels.append(content[1:])
-    return img_paths, labels
+ads = {'0' : '_', '1' : 'A', '2' : 'B', '3' : 'C', '4' : 'D', '5' : 'E',
+       '6' : 'F', '7' : 'G', '8' : 'H', '9' : 'I', '10' : 'J',
+       '11' : 'K', '12' : 'L', '13' : 'M', '14' : 'N', '15' : 'O',
+       '16' : 'P', '17' : 'Q', '18' : 'R', '19' : 'S', '20' : 'T',
+       '21' : 'U', '22' : 'V', '23' : 'W', '24' : 'X', '25' : 'Y',
+       '26' : 'Z', '27' : '0', '28' : '1', '29' : '2', '30' : '3',
+       '31' : '4', '32' : '5', '33' : '6', '34' : '7', '35' : '8',
+       '36' : '9'}
 
-class LpLocDataLoader(Dataset):
-    def __init__(self, img_dir,imgSize, is_transform=None):
-        self.img_dir = img_dir
-        self.img_paths, self.labels = load_path(self.img_dir)
-        self.img_size = imgSize
-        self.is_transform = is_transform
+provNum = {j : i for i, j in ads.items()}
 
-    def __len__(self):
-        return len(self.img_paths)
-
-    def __getitem__(self, index):
-        img_name = self.img_paths[index]
-        img = cv2.imread(join(self.img_dir, img_name))
-        resizedImage = cv2.resize(img, self.img_size)
-        resizedImage = np.reshape(resizedImage, (resizedImage.shape[2], resizedImage.shape[0], resizedImage.shape[1]))
-
-        ori_w, ori_h = float(img.shape[1]), float(img.shape[0])
-        #assert img.shape[0] == 1160
-        label = self.labels[index]
-        new_labels = [(int(label[0]) + int(label[2]))/(2*ori_w), (int(label[1]) + int(label[3]))/(2*ori_h), (int(label[2])-int(label[0]))/ori_w, (int(label[3])-int(label[1]))/ori_h]
-
-        resizedImage = resizedImage.astype('float32')
-        # Y = Y.astype('int8')
-        resizedImage /= 255.0
-        # lbl = img_name.split('.')[0].rsplit('-',1)[-1].split('_')[:-1]
-        # lbl = img_name.split('/')[-1].split('.')[0].rsplit('-',1)[-1]
-        # lbl = map(int, lbl)
-        # lbl2 = [[el] for el in lbl]
-
-        # resizedImage = torch.from_numpy(resizedImage).float()
-        return resizedImage, new_labels
-
-lpnet = lpnet(numPoints, numClasses, model_name= 'base', net_file= None, base_file = None)
-dst = LpLocDataLoader('img', imgSize)
-trainloader = DataLoader(dst, batch_size=batchSize, shuffle=True, num_workers=4)
-lpnet.train(trainloader, batchSize, epoch_start= 0, epochs= 25)
+lpnet = lpnet(numPoints, numClasses, model_name = model_name, net_file = None, base_file = None)
+dst = lpDataLoader(img_dir, input_shape, provNum, model_name = model_name)
+trainloader = DataLoader(dst, batch_size=batchSize, shuffle=True, num_workers= num_workers)
+lpnet.train(trainloader, batchSize, epoch_start= 0, epochs= epochs)
